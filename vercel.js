7d1ef32acb429f6a23441ef9d687517e098d54b4
Adrian.dev <adrianmogus@hotmail.com>
2025-03-26 23:26:42 -0300

    Primer commit de mi frontend

--- vercel.js
+++ vercel.js
@@ -1,6 +1,6 @@
 {
     "rewrites": [
-      { "source": "/api/(.*)", "destination": "https://tu-backend.vercel.app/api/$1" }
+      { "source": "/api/(.*)", "destination": "https://utn-pwa-febrero-tn-fullstack-ecommerce-backend.vercel.app/api/$1" }
     ],
     "redirects": [
       { "source": "/(.*)", "destination": "/", "statusCode": 200 }
