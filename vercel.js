5bc70aab0c0188b41f7c3e33c7400b9318597611
Adrian.dev <adrianmogus@hotmail.com>
2025-03-28 16:15:19 -0300

    Primer commit de mi frontend

--- vercel.js
+++ vercel.js
@@ -1,9 +1,18 @@
-{
-    "rewrites": [
-      { "source": "/api/(.*)", "destination": "https://utn-pwa-febrero-tn-fullstack-ecommerce-backend.vercel.app/api/$1" }
-    ],
-    "redirects": [
-      { "source": "/(.*)", "destination": "/", "statusCode": 200 }
-    ]
-  }
-  
+module.exports = {
+  // Rewrites para redirigir las rutas de la API
+  rewrites: [
+    {
+      source: '/api/(.*)',
+      destination: 'https://utn-pwa-febrero-tn-fullstack-ecommerce-backend.vercel.app/api/$1',
+    },
+  ],
+
+  // Redirección de todas las rutas a index.html para manejar el enrutamiento con React (SPA)
+  redirects: [
+    {
+      source: '/(.*)',
+      destination: '/index.html',
+      statusCode: 200,
+    },
+  ],
+};
