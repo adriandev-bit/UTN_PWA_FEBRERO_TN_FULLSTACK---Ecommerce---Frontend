d80360596509f7d10e13566260b1b6f08de21f37
Adrian.dev <adrianmogus@hotmail.com>
2025-03-27 22:48:59 -0300

    Primer commit de mi frontend

--- src/Screens/ProductDetail.jsx
+++ src/Screens/ProductDetail.jsx
@@ -11,7 +11,7 @@ const ProductDetail = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    
+    window.scrollTo(0, 0); // Mueve la página al inicio
     if (productId) {
       fetchProductDetails();
       fetchReviews();
